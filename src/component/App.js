8a1877c300c171e9d258701f598200d20ef9ff59
YafeiQQ <yafeigeng1228@gmail.com>
2019-07-06 15:15:34 -0700

    rename

---
+++ src/component/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+
+function App() {
+    return (
+        <div>
+            <Button variant="contained" color="primary">
+                发送
+            </Button>
+            <Button variant="contained" color="primary">
+                预览
+            </Button>
+            <Button variant="contained" color="primary">
+                附件
+            </Button>
+        </div>
+
+    );
+}
+
+export default App;
