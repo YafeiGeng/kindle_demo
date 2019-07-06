e46903020e16813e056a003f7f29063a3eebed01
YafeiQQ <yafeigeng1228@gmail.com>
2019-07-06 16:15:41 -0700

    refactor

---
+++ src/component/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AppBar from './AppBar.js';
+import '../style/App.scss';
+import AppMain from "./AppMain";
+
+function App() {
+    return (
+        <div>
+            <AppBar/>
+            <AppMain/>
+        </div>
+    );
+}
+
+export default App;
